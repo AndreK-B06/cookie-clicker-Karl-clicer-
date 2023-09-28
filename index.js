932a0bc737d0cad3aa63506bc2fe5dd61e6333a5
AndreK-B06 <andrekb06.kodehode@gmail.com>
2023-09-28 12:43:14 +0200

    Upgrade Curser price fix

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
 const tempI = document.querySelector("#temp");
 const moneyEl = document.querySelector("#money-el");
-let curserPrice = document.querySelector("#price-curser-el");
+let curserEL = document.querySelector("#curser-el");
 
 let money = 0;
 let clickStreng = 1;
-let krCurser = 2;
+let krCurser = 5;
+const priceIncres = 2;
 
 tempI.addEventListener("click", function () {
   money = clickStreng + money;
@@ -13,16 +14,21 @@ tempI.addEventListener("click", function () {
 /* -----------------------------------
 !!!!!!!!!!!!! Curser Upgrade !!!!!!!!!
 --------------------------------------*/
+
 function upgradeCurser() {
   if (money === krCurser) {
     money -= krCurser;
-    moneyEl.innerHTML = `${money} Kr.`;
-    krCurser *= krCurser;
-    //priceCurserEL.innerHTML = `${curserPrice} Kr`;
+    krCurser *= priceIncres;
+    curserEL.textContent = `${krCurser} Kr`;
     clickStreng += 1;
+    moneyEl.innerHTML = `${money} Kr.`;
   } else if (money >= krCurser) {
     money -= krCurser;
-    moneyEl.innerHTML = `${money} Kr.`;
+    krCurser *= priceIncres;
+    curserEL.textContent = `${krCurser} Kr`;
     clickStreng += 1;
+    moneyEl.innerHTML = `${money} Kr.`;
   }
 }
+
+// charImageEl.style.width = `${50 * factor}%`;
