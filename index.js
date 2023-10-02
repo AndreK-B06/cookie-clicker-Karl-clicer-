036bb8a38f6686c04ba4eca7a9cde89db232c1b9
AndreK-B06 <andrekb06.kodehode@gmail.com>
2023-10-02 09:41:36 +0200

    added "fi" "ellse"

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const tempI = document.querySelector("#temp");
 const moneyEl = document.querySelector("#money-el");
 let curserEL = document.querySelector("#curser-el");
+let myImage = document.querySelector("img");
 
 let karlImages = ["/image/Karl-ok.png", "/image/Karl-nei.png"];
 
@@ -8,9 +9,20 @@ let money = 0;
 let clickStreng = 1;
 let krCurser = 5;
 const priceIncres = 2;
+
+/* -----------------------------------
+!!!!!!!!!!!!! On click !!!!!!!!!!!!!!!
+--------------------------------------*/
+
 tempI.addEventListener("click", function () {
   money = clickStreng + money;
   moneyEl.textContent = `${money} Kr.`;
+  let changeImg = myImage.getAttribute("src");
+  if (changeImg === "/image/Karl-ok.png") {
+    myImage.setAttribute("scr", "/image/Karl-nei.png");
+  } else {
+    myImage.setAttribute("src", "image/karl-ok.png");
+  }
 });
 
 /* -----------------------------------
