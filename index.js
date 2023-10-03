c7a8e8d8e098ac0cdb5c8824e861f79ceeb0a1f9
AndreK-B06 <andrekb06.kodehode@gmail.com>
2023-10-03 12:29:56 +0200

    Audio added (not finised)

--- index.js
+++ index.js
@@ -1,35 +1,54 @@
 const tempI = document.querySelector("#temp");
 const moneyEl = document.querySelector("#money-el");
-let curserEL = document.querySelector("#curser-el");
 let myImage = document.querySelector("img");
 
-let karlImages = ["/image/Karl-ok.png", "/image/Karl-nei.png"];
+// let karlImages = ["/image/karl-ok.png", "/image/narl-nei.png"];
 
 let money = 0;
 let clickStreng = 1;
-let krCurser = 5;
 const priceIncres = 2;
 
 /* -----------------------------------
 !!!!!!!!!!!!! On click !!!!!!!!!!!!!!!
 --------------------------------------*/
-
+//  Jobbes forsat med å fikse på å bytte bilde
 tempI.addEventListener("click", function () {
   money = clickStreng + money;
   moneyEl.textContent = `${money} Kr.`;
   let changeImg = myImage.getAttribute("src");
-  if (changeImg === "./image/Karl-ok.png") {
-    myImage.setAttribute("scr", "./image/Karl-nei.png");
+  if (changeImg === "./image/karl-ok.png") {
+    myImage.setAttribute("scr", "./image/karl-nei.png");
     console.log("karl nei");
-  } else {
+  } else if (changeImg === "./image/karl-nei.png") {
     myImage.setAttribute("src", "./image/karl-ok.png");
     console.log("karl ok");
+  } else {
+    console.log(Error);
   }
 });
+/* -----------------------------------
+!!!!!!!!!!!!! Upgrade !!!!!!!!!
+--------------------------------------*/
+
+/* -----------------------------------
+!!!!!!!!!!!!! Audio !!!!!!!!!
+--------------------------------------*/
+const clikAudio = {
+  folder: "audio/",
+  sounds: [
+    { file: "goofy.mp3", name: "goofy" },
+    { file: "grill.mp3", name: "grill" },
+    { file: "hold-kjeft.mp3", name: "hold-kjeft" },
+    { file: "teleporte.mp3", name: "teleporte" },
+  ],
+};
+const upgradeComtainer = document.querySelector("#audio-click");
 
 /* -----------------------------------
 !!!!!!!!!!!!! Curser Upgrade !!!!!!!!!
 --------------------------------------*/
+let curserEL = document.querySelector("#curser-el");
+let krCurser = 5;
 
 function upgradeCurser() {
   if (money === krCurser) {
@@ -44,5 +63,25 @@ function upgradeCurser() {
     curserEL.textContent = `${krCurser} Kr`;
     clickStreng += 1;
     moneyEl.innerHTML = `${money} Kr.`;
+  }
+}
+/* -----------------------------------
+!!!!!!!!!!!!! Grill Upgrade !!!!!!!!!
+--------------------------------------*/
+let grillEl = document.querySelector("#grill");
+let krGrill = 100;
+function upgradeGrill() {
+  if (money == krGrill) {
+    money -= krGrill;
+    krGrill *= priceIncres;
+    grillEl.textContent = `${krGrill} Kr`;
+    clickStreng += 20;
+    moneyEl.innerHTML = `${money} Kr`;
+  } else if (money >= krGrill) {
+    money -= krGrill;
+    krGrill *= priceIncres;
+    grillEl.textContent = `${krGrill} Kr`;
+    clickStreng += 20;
+    moneyEl.innerHTML = `${money} Kr`;
   }
 }
