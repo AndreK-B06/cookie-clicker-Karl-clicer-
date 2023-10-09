6993e9e7e6bc49cab09df54fb3b89c067ae3b893
AndreK-B06 <andrekb06.kodehode@gmail.com>
2023-10-09 11:08:29 +0200

    Autio fikx

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const tempI = document.querySelector("#temp");
+const imgKarl = document.querySelector("#img-karl");
 const moneyEl = document.querySelector("#money-el");
 let myImage = document.querySelector("img");
 
@@ -11,20 +11,27 @@ const priceIncres = 2;
 /* -----------------------------------
 !!!!!!!!!!!!! On click !!!!!!!!!!!!!!!
 --------------------------------------*/
+const imageChange = document.querySelector(".img-change");
+let clickKarlChange = 1;
+imageChange.addEventListener("click", function () {
+  if (clickKarlChange == 1) {
+  }
+});
 //  Jobbes forsat med å fikse på å bytte bilde
-tempI.addEventListener("click", function () {
+imgKarl.addEventListener("click", function () {
   money = clickStreng + money;
   moneyEl.textContent = `${money} Kr.`;
-  let changeImg = myImage.getAttribute("src");
-  if (changeImg === "./image/karl-ok.png") {
-    myImage.setAttribute("scr", "./image/karl-nei.png");
-    console.log("karl nei");
-  } else if (changeImg === "./image/karl-nei.png") {
-    myImage.setAttribute("src", "./image/karl-ok.png");
-    console.log("karl ok");
-  } else {
-    console.log("Error");
-  }
+
+  // let changeImg = myImage.getAttribute("src");
+  // if (changeImg === "./image/karl-ok.png") {
+  //   myImage.setAttribute("scr", "./image/karl-nei.png");
+  //   console.log("karl nei");
+  // } else if (changeImg === "./image/karl-nei.png") {
+  //   myImage.setAttribute("src", "./image/karl-ok.png");
+  //   console.log("karl ok");
+  // } else {
+  //   console.log("Error");
+  // }
 });
 /* -----------------------------------
 !!!!!!!!!!!!! Upgrade !!!!!!!!!
@@ -49,14 +56,14 @@ function upgradeCurser() {
     curserEl.textContent = `${krCurser} Kr`;
     clickStreng += 1;
     moneyEl.innerHTML = `${money} Kr.`;
-    audioHoldKjeftEl.play();
+    audioHoldKjeftEl.play(0);
   } else if (money >= krCurser) {
     money -= krCurser;
     krCurser *= priceIncres;
     curserEl.textContent = `${krCurser} Kr`;
     clickStreng += 1;
     moneyEl.innerHTML = `${money} Kr.`;
-    audioHoldKjeftEl.play();
+    audioHoldKjeftEl.play(0);
   }
 }
 /* -----------------------------------
@@ -71,13 +78,13 @@ function upgradeGrill() {
     grillEl.textContent = `${krGrill} Kr`;
     clickStreng += 20;
     moneyEl.innerHTML = `${money} Kr`;
-    audioGrillEl.play();
+    audioGrillEl.play(0);
   } else if (money >= krGrill) {
     money -= krGrill;
     krGrill *= priceIncres;
     grillEl.textContent = `${krGrill} Kr`;
     clickStreng += 20;
     moneyEl.innerHTML = `${money} Kr`;
-    audioGrillEl.play();
+    audioGrillEl.play(0);
   }
 }
